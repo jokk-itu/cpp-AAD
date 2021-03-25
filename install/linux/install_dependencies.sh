93cc268d2753586fa19182314e3f3b913d76d551
Joachim Køcher Kelsen <jokk@itu.dk>
2021-03-25 11:22:06 +0100

    Edit main.yml \ \ Add install script, to install dependencies

---
+++ install/linux/install_dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# INSTALL TESTING DEPENDENCY
+git clone https://github.com/google/googletest.git -b release-1.10.0
+cd googletest
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
